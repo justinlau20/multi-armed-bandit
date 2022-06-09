6e87b12310c0aadaa8cbb8411b924465c3600ddc
Lunzhi Shi <ls1420@ic.ac.uk>
2022-06-09 21:23:18 +0100

    add UCB feature to greedy bayesian method

--- infrastructure/greedy_bayesian_bernoulli.py
+++ infrastructure/greedy_bayesian_bernoulli.py
@@ -16,6 +16,7 @@ from scipy.stats import beta
 import matplotlib.pyplot as plt
 from copy import copy
 import random
+from math import sqrt
 
 # initialize machines and beta priors (uniform dist.)
 machines = [bernoulli_machine(i) for i in [0.33, 0.55, 0.6]]  # noqa: F405
@@ -25,10 +26,11 @@ priors = [[1, 1] for i in range(len(machines))]
 class GreedyBayesianBernoulli(Game):
     """Greedy Bayesian using beta prior."""
 
-    def __init__(self, prior_parameters, threshold, turns, *machines):
+    def __init__(self, prior_parameters, threshold, ucb, turns, *machines):
         super().__init__(turns, *machines)
         self.parameters = copy([copy(sublist) for sublist in prior_parameters])
         self.threshold = threshold
+        self.ucb = ucb
 
     def _update(self, index, outcome):
         super()._update(index, outcome)
@@ -42,11 +44,16 @@ class GreedyBayesianBernoulli(Game):
         e = random.uniform(0, 1)
         pre_mean = [beta[0] / (beta[0] + beta[1])
                     for beta in self.parameters]
+        pre_std = [sqrt(beta[0] * beta[1] / ((beta[0] + beta[1]) ** 2
+                   + (beta[0] + beta[1] + 1))) for beta in self.parameters]
+        pre_ucb = [pre_mean[i] + self.ucb * pre_std[i]
+                   for i in range(len(self.parameters))]
+
         if e > self.threshold:
-            decision_index = np.argmax(pre_mean)
+            decision_index = np.argmax(pre_ucb)
         else:
             index = [i for i, _ in enumerate(machines)]
-            index.pop(np.argmax(pre_mean))
+            index.pop(np.argmax(pre_ucb))
             decision_index = random.choice(index)
 
         return decision_index
@@ -59,7 +66,7 @@ def plot_beta_pdf(ax, a, b):
 
 
 turns = [10, 50, 100, 1000]
-g = [GreedyBayesianBernoulli(priors, 0.02, i, *machines) for i in turns]
+g = [GreedyBayesianBernoulli(priors, 0.05, 1, i, *machines) for i in turns]
 for g_turn in g:
     g_turn.simulate()
 
