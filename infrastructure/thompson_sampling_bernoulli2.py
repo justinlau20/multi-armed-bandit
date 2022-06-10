c48c891252b2984e721b63ff3d54d39f5b1aa5a2
Colman-spelt-without-an-E <106886906+Colman-spelt-without-an-E@users.noreply.github.com>
2022-06-10 12:47:09 +0100

    docstring

--- infrastructure/thompson_sampling_bernoulli2.py
+++ infrastructure/thompson_sampling_bernoulli2.py
@@ -13,29 +13,47 @@ machines = [bernoulli_machine(i) for i in [0.33, 0.55, 0.6]]
 class ThompsonSamplingBernoulli(Game):
     """
     Thompson sampling using beta prior.
+    
+    Parameters
+    ----------
+        prior_parameters : list
+            Nested list containing the beta prior parameters for each machine
+            e.g. [[1,1], [1,1], [1,1]]
+        turns : int
+            Number of turns to be played
+        *machines : list
+            List of Machines
     """
     
     # add posterior parameters
     def __init__(self, prior_parameters, turns, *machines):
+        """
+        Constructs attributes. 
+        
+        Attributes
+        ----------
+            post_parameters : list
+                Nested list containing the beta posterior parameters for each machine after each turn
+            post_parameters_history : list
+                Stores the beta posterior parameters for each machine and each turn
+        """
         super().__init__(turns, *machines)
         self.post_parameters = prior_parameters
         self.post_parameters_history = [deepcopy(prior_parameters)]
         
     # overwrite decide
     def decide(self):
+        
         # beta posterior distribution parameters
         post_samples = []
         for a, b in self.post_parameters:
             post_samples.append(np.random.beta(a, b))
-#         print(f"posterior parameters: {self.post_parameters}")
-#         print(f"posterior samples: {post_samples}")
-#         print(f"posterior parameters history: {self.post_parameters_history}")
         return np.argmax(post_samples)
     
     # overwrite _update to store posterior parameters
     def _update(self, index, outcome):
         super()._update(index, outcome)
-#         print(f"machine output: {outcome}")
+        
         # update the posterior distribution at given index
         if outcome == 1:
             self.post_parameters[index][0] += 1
@@ -45,7 +63,7 @@ class ThompsonSamplingBernoulli(Game):
 
 # α, β parameters for beta prior
 # α = β = 1 gives uniform distribution
-priors = [[[1,1]] for i in range(len(machines))]
+priors = [[1,1] for i in range(len(machines))]
 
 g = ThompsonSamplingBernoulli(priors, 1000, *machines)
 g.simulate()
