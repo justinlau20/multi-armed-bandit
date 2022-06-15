dc179cdeb659b56e8d67383b56ec3a01fc8965e0
Colman-spelt-without-an-E <106886906+Colman-spelt-without-an-E@users.noreply.github.com>
2022-06-15 15:59:19 +0100

    Update ts_rpm_analysis.py

--- infrastructure/ts_rpm_analysis.py
+++ infrastructure/ts_rpm_analysis.py
@@ -1,6 +1,6 @@
 from infrastructure import *
 from RPM import RPMBernoulli
-from TS import ThompsonSamplingBernoulli
+from thompson_bernoulli_no_output import ThompsonSamplingBernoulli
 import matplotlib.pyplot as plt
 import dill
 import bz2
@@ -15,13 +15,14 @@ with bz2.open('ts_n100_T5000.bz2', 'rb') as handle:
 with bz2.open('rpm_n100_T5000.bz2', 'rb') as handle:
     rpm_games2 = dill.load(handle)
 
-n = 100     #no of simulations
-T = 5000    #horizon
+n = 100     # no of simulations
+T = 5000    # horizon
 
+# compare cumulative regret
 ts_regret = np.array([ts_game.historical_regret for ts_game in ts_games2])
 rpm_regret = np.array([rpm_game.historical_regret for rpm_game in rpm_games2])
 
-# compare cumulative regret
+# plot averaged cumulative regret with 95% confidence interval
 fig, ax = plt.subplots()
 ax.plot(range(T+1), np.mean(ts_regret, axis=0), color = 'red')
 ax.plot(range(T+1), np.mean(rpm_regret, axis=0), color = 'black')
